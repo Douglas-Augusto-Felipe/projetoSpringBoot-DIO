44d446ea54f84f16ff19d8c0ec002e873ddc533d
Douglas-Augusto <douglas.full.ti@gmail.com>
2023-10-04 04:13:16 -0300

    Initial commit

---
+++ src/main/java/dio/projetoSpringBootDIO/ProjetoSpringBootDioApplication.java
@@ -0,0 +1,13 @@
+package dio.projetoSpringBootDIO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringBootDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringBootDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/projetoSpringBootDIO/ProjetoSpringBootDioApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.projetoSpringBootDIO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringBootDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
